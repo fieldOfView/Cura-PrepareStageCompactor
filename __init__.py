c7f7a0546b66ab15d518f6e3fe0a466fcfa599c3
fieldOfView <aldo@fieldofview.com>
2018-04-26 09:23:55 +0200

    Match the plugin version to the Cura version before loading the plugin

--- __init__.py
+++ __init__.py
@@ -1,14 +1,46 @@
 # Copyright (c) 2018 fieldOfView
 # The PrepareStageCompactor is released under the terms of the AGPLv3 or higher.
 
+import os, json
+
 from . import PrepareStageCompactor
 from UM.i18n import i18nCatalog
 i18n_catalog = i18nCatalog("cura")
 
+from UM.Version import Version
+from UM.Application import Application
+from UM.Logger import Logger
+
 def getMetaData():
-	return {}
+    return {}
 
 def register(app):
-    return {
-        "extension": PrepareStageCompactor.PrepareStageCompactor()
-    }
+    if __matchVersion():
+        return {
+            "extension": PrepareStageCompactor.PrepareStageCompactor()
+        }
+    else:
+        Logger.log("w", "Plugin not loaded because of a version mismatch")
+        return {}
+
+def __matchVersion():
+    cura_version = Application.getInstance().getVersion()
+    if cura_version == "master":
+        Logger.log("d", "Running Cura from source, ignoring version of the plugin")
+        return True
+    cura_version = Version(cura_version)
+
+    # Get version information from plugin.json
+    plugin_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "plugin.json")
+    try:
+        with open(plugin_file_path) as plugin_file:
+            plugin_info = json.load(plugin_file)
+            plugin_version = Version(plugin_info["version"])
+    except:
+        Logger.log("w", "Could not get version information for the plugin")
+        return False
+
+    if plugin_version.getMajor() == cura_version.getMajor() and plugin_version.getMinor() == cura_version.getMinor():
+        return True
+    else:
+        return False
